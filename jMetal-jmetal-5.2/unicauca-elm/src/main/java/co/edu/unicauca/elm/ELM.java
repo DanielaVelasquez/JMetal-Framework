318d2e84688895eb1c01f1e2899d9654d8ad6737
DanielaVG <angiedanielav@unicauca.edu.co>
2017-05-28 12:50:00 -0500

    MAtrix Util created for random matrix using jmetal

--- jMetal-jmetal-5.2/unicauca-elm/src/main/java/co/edu/unicauca/elm/ELM.java
+++ jMetal-jmetal-5.2/unicauca-elm/src/main/java/co/edu/unicauca/elm/ELM.java
@@ -1,14 +1,114 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package co.edu.unicauca.elm;
 
-/**
- *
- * @author Daniela
- */
-public class ELM {
+import no.uib.cipr.matrix.DenseMatrix;
+import no.uib.cipr.matrix.DenseVector;
+import no.uib.cipr.matrix.NotConvergedException;
+
+public class ELM 
+{
+    /**
+     * ELM Type for classification and regression
+     */
+    public enum ELMType
+    {
+        REGRESSION,
+        CLASSIFICATION
+    }
+    /**
+     * Activation function - Type of activation function: 'sig' for
+     * Sigmoidal function 'sin' for Sine function 'hardlim' for Hardlim function
+     * 'tribas' for Triangular basis function 'radbas' for Radial basis function
+     */
+    public enum ActivationFunction
+    {
+        SIG,
+        SIN,
+        HARDLIM,
+        TRIBAS,
+        RADBAS
+    }
+    /**-----------------------------------------------------------------------------------------
+     * Atributes
+     *-----------------------------------------------------------------------------------------*/
+    
+    /**
+     *Activation Function  
+     */
+    private ActivationFunction function;
+    /**
+     * Time while testing (in seconds)
+     */
+    private double testing_time;
+    /**
+     * ELM accuracy while testing
+     */
+    private double testing_accuracy;
+    /**
+     * Time while training (in seconds)
+     */
+    private double training_time;
+    /**
+     * ELM accuracy while training
+     */
+    private double training_accuracy;    
+    /**
+     * ELM type (Regression or Classification)
+     */    
+    private ELMType elm_type;
+    /**
+     * Number of input neurons
+     */
+    private int input_neurons;
+    /**
+     * Number of hidden neurons assigned to the ELM
+     */
+    private int hidden_neurons;
+    /**
+     * Number of output neurons
+     */
+    private int output_neurons;
+    /**
+     * Matrix bias of hidden neurons
+     */
+    private DenseMatrix bias_hidden_neurons;
+    /**
+     * Matrix input weight - Weights between input layer and the hidden layer
+     * w_ij is the weight between neuron i (in input layer) and neuron j (in the hidden layer)
+     */
+    private DenseMatrix input_weight;
+    /**
+     * Matrix output weight - Weights between the hidden layer and output layer 
+     * b_jk represents the weight between the neuron j (in the hidden layer) and neuron k (in output layer)
+     */
+    private DenseMatrix output_weight;
+    
+    /**-----------------------------------------------------------------------------------------
+     * Methods
+     *-----------------------------------------------------------------------------------------*/
+    /**
+     * Construct an ELM
+     * @param elm_type Elm type - classification or regression
+     * @param hidden_neurons Number of hidden neurons assigned to the ELM
+     * @param activation_function Type of activation function - Type of activation function: 'sig' for
+     * Sigmoidal function 'sin' for Sine function 'hardlim' for Hardlim function
+     * 'tribas' for Triangular basis function 'radbas' for Radial basis function
+     */
+    public ELM(ELMType elm_type,int hidden_neurons,ActivationFunction activation_function)
+    {
+        this.elm_type = elm_type;
+        this.hidden_neurons = hidden_neurons;
+        this.function = activation_function;
+        this.training_time = 0;
+        this.testing_time = 0;
+        this.training_accuracy = 0;
+        this.testing_accuracy = 0;
+        this.output_neurons = 0;
+    }
+    
+    public void train()
+    {
+        if(input_weight == null)
+            
+    }
     
 }
---
+++ jMetal-jmetal-5.2/unicauca-elm/src/main/java/co/edu/unicauca/elm/util/MatrixUtil.java
@@ -0,0 +1,26 @@
+package co.edu.unicauca.elm.util;
+
+import no.uib.cipr.matrix.DenseMatrix;
+import org.uma.jmetal.util.pseudorandom.JMetalRandom;
+
+
+public class MatrixUtil 
+{
+    /**
+     * Randomly fill a matrix with a specific size
+     * @param rows Number of rows 
+     * @param cols Number of columns
+     * @param min_value Minimum value allowed in the matrix
+     * @param max_value Max value allowed in the matrix
+     * @return Matrix with 'rows' number of rows and 'cols' number of columns and random values
+     */
+    public static DenseMatrix randomFill(int rows, int cols,double min_value, double max_value)
+    {
+        DenseMatrix matrix = new DenseMatrix(rows, cols);
+        JMetalRandom rnd = JMetalRandom.getInstance();
+        for(int i = 0; i<rows;i++)
+            for(int j = 0; j < cols; j++)
+                matrix.set(i, j, rnd.nextDouble(min_value, max_value));
+        return matrix;
+    }
+}
