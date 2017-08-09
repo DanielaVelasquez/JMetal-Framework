c6ce580655b1f9059d5be9741721fa19851c16ab
Daniela Velásquez <angiedanielav@unicauca.edu.co>
2017-08-08 20:50:09 -0500

    Exec deccg

--- jMetal-jmetal-5.2/jmetal-exec/src/main/java/org/uma/jmetal/runner/singleobjective/IrisRunner.java
+++ jMetal-jmetal-5.2/jmetal-exec/src/main/java/org/uma/jmetal/runner/singleobjective/IrisRunner.java
@@ -1,8 +1,8 @@
-package org.uma.jmetal.runner.singleobjective;
+package co.edu.unicauca.exec.training_testing;
 
-
+import java.util.Comparator;
 import org.uma.jmetal.algorithm.Algorithm;
-import org.uma.jmetal.algorithm.singleobjective.differentialevolution.SaNSDE;
+import org.uma.jmetal.algorithm.singleobjective.differentialevolution.DECC_G;
 import org.uma.jmetal.operator.impl.crossover.DifferentialEvolutionCrossover;
 import org.uma.jmetal.operator.impl.selection.DifferentialEvolutionSelection;
 import org.uma.jmetal.problem.DoubleProblem;
@@ -16,20 +16,21 @@ import org.uma.jmetal.util.pseudorandom.JMetalRandom;
 
 
 
+
 public class IrisRunner 
 {
-
+    
     public static void main(String[] args) throws Exception {
         JMetalRandom rnd = JMetalRandom.getInstance();
-        rnd.setSeed(1);
+        rnd.setSeed(10);
         DoubleProblem problem;
         Algorithm<DoubleSolution> algorithm;
         DifferentialEvolutionSelection selection;
         DifferentialEvolutionCrossover crossover;
-
         SolutionListEvaluator<DoubleSolution> evaluator ;
         String problemName ;
         String referenceParetoFront = "" ;
+        Comparator<DoubleSolution> comparator;
         if (args.length == 1) {
           problemName = args[0];
         } else if (args.length == 2) {
@@ -40,8 +41,8 @@ public class IrisRunner
         }
         evaluator = new SequentialSolutionListEvaluator<DoubleSolution>() ;
         problem = (DoubleProblem) ProblemUtils.<DoubleSolution> loadProblem(problemName);
-        
-        
+        comparator = new ObjectiveComparator<DoubleSolution>(0,ObjectiveComparator.Ordering.ASCENDING);
+       
         /*double result[] = new double[]{-0.8644811116014093,0.482980034535944,-1.0,-1.0,0.9693794184155992,-0.6966032329569359,0.29491871745966697,0.5174827338123161,-0.915489247034809,-0.7829404115403313,0.21532418070938986,0.6453706265910443,-0.9656326548227394,-1.0,-1.0,-0.8573846664773566,-1.0,-0.9743262421676628};
         
         
@@ -56,10 +57,7 @@ public class IrisRunner
 
         selection = new DifferentialEvolutionSelection() ;
 
-        algorithm = new SaNSDE(problem, 100000, 100,
-        crossover, new DifferentialEvolutionCrossover(cr, f, "current-to-best/1/bin"), 
-        selection, evaluator,
-        new ObjectiveComparator<DoubleSolution>(0));
+        algorithm = new DECC_G(20,30,100,100,problem,10,evaluator, comparator);
         
 
         AlgorithmRunner algorithmRunner = new AlgorithmRunner.Executor(algorithm)
@@ -69,15 +67,14 @@ public class IrisRunner
         long computingTime = algorithmRunner.getComputingTime() ;
 
         System.out.println("Total execution time: " + computingTime + "ms");
+        System.out.println("Objective "+(1-solution.getObjective(0)));
         /*double a = ((TrainingTestingEvaluator)problem).test(solution);
         
         System.out.println("Accuracy "+a);
-
         /*printFinalSolutionSet(population);
         if (!referenceParetoFront.equals("")) {
           printQualityIndicators(population, referenceParetoFront) ;
         }*/
     }
     
-
 }
