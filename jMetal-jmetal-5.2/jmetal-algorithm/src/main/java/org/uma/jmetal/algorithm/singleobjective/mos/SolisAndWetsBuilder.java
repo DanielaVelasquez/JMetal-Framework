eabaf5ccf36037b44e1674ef32c8068e8d9be9fb
David Sotelo <davidstl4@gmail.com>
2017-09-16 22:03:05 -0500

    Solis And Wets terminado pero no probado

---
+++ jMetal-jmetal-5.2/jmetal-algorithm/src/main/java/org/uma/jmetal/algorithm/singleobjective/mos/SolisAndWetsBuilder.java
@@ -0,0 +1,62 @@
+package org.uma.jmetal.algorithm.singleobjective.mos;
+
+import java.util.Comparator;
+import org.uma.jmetal.problem.DoubleProblem;
+import org.uma.jmetal.solution.DoubleSolution;
+import org.uma.jmetal.util.JMetalException;
+import org.uma.jmetal.util.comparator.ObjectiveComparator;
+
+public class SolisAndWetsBuilder 
+{
+    private DoubleProblem problem;
+    private Comparator<DoubleSolution> comparator;
+    private int sizeNeighborhood;
+    private double rho;
+    private int numCycles;
+    
+    public SolisAndWetsBuilder(DoubleProblem problem)
+    {
+        this.problem = problem;
+        this.comparator = new ObjectiveComparator<DoubleSolution>(0,ObjectiveComparator.Ordering.ASCENDING);
+        this.sizeNeighborhood = 20;
+        this.rho = 0.5;
+        this.numCycles = 5;
+    }
+    
+    public SolisAndWetsBuilder setPopulationSizeNeighborhoodSize(int populationSizeNeighborhood)
+    {
+        if(populationSizeNeighborhood < 0)
+        {
+            throw new JMetalException("Neighborhood size is negative: " + populationSizeNeighborhood);
+        }
+        else if(populationSizeNeighborhood % 2 != 0)
+        {
+            throw new JMetalException("Neighborhood size should be even: " + populationSizeNeighborhood);
+        }
+        
+        this.sizeNeighborhood = populationSizeNeighborhood;
+        return this;
+    }
+    
+    public SolisAndWetsBuilder setNumCycles(int numCycles)
+    {
+        if(numCycles < 0)
+        {
+            throw new JMetalException("Cycles is negative: " + numCycles);
+        }
+        
+        this.numCycles = numCycles;
+        return this;
+    }
+    
+    public SolisAndWetsBuilder setRho(double rho)
+    {
+        this.rho = rho;
+        return this;
+    }
+    
+    public SolisAndWets build()
+    {
+        return new SolisAndWets(problem, comparator, numCycles, rho, sizeNeighborhood);
+    }
+}
