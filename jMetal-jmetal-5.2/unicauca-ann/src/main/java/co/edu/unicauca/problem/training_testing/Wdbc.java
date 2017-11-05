d862c205dcc2e56b026819c3100d7f5a5c786860
Daniela Velásquez <angiedanielav@unicauca.edu.co>
2017-11-05 18:15:46 -0500

    cambios de las neuronas tt

--- jMetal-jmetal-5.2/unicauca-ann/src/main/java/co/edu/unicauca/problem/training_testing/Wdbc.java
+++ jMetal-jmetal-5.2/unicauca-ann/src/main/java/co/edu/unicauca/problem/training_testing/Wdbc.java
@@ -1,16 +1,14 @@
 package co.edu.unicauca.problem.training_testing;
 
-import co.edu.unicauca.problem.cross_validation.*;
 import co.edu.unicauca.dataset.DataSet;
 import co.edu.unicauca.elm_function.impl.Sigmoid;
 import co.edu.unicauca.moore_penrose.impl.RidgeRegressionTheory;
-import co.edu.unicauca.problem.AbstractELMEvaluator;
 import java.io.IOException;
 
 public class Wdbc extends TrainingTestingEvaluator {
 
     public Wdbc() throws IOException {
-        super(20, new DataSet("src/resources-elm", "wdbc.train", 30), new DataSet("src/resources-elm", "wdbc.test", 30), new Sigmoid(), new RidgeRegressionTheory(new double[]{0,000000001}), "Wdbc", 3000);
+        super(50, new DataSet("src/resources-elm", "wdbc.train", 30), new DataSet("src/resources-elm", "wdbc.test", 30), new Sigmoid(), new RidgeRegressionTheory(new double[]{0,000000001}), "Wdbc", 3000);
     }
 
 }
