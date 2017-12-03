9dfa5b851950082ee5f07bf4850d223876ccac09
Daniela Velásquez <angiedanielav@unicauca.edu.co>
2017-12-02 19:24:56 -0500

    cambios en mos

--- jMetal-jmetal-5.2/unicauca-ann/src/main/java/co/edu/unicauca/exec/training_testing/MOSRunner.java
+++ jMetal-jmetal-5.2/unicauca-ann/src/main/java/co/edu/unicauca/exec/training_testing/MOSRunner.java
@@ -1,19 +1,17 @@
 package co.edu.unicauca.exec.training_testing;
 
 import co.edu.unicauca.problem.AbstractELMEvaluator;
-import java.util.HashMap;
 import org.uma.jmetal.algorithm.singleobjective.mos.MOSBuilder;
 import org.uma.jmetal.algorithm.singleobjective.mos.MOSHRH;
 import org.uma.jmetal.algorithm.singleobjective.mos.MTSLS1Tecnique;
-import org.uma.jmetal.algorithm.singleobjective.mos.MTSTecnique;
 import org.uma.jmetal.algorithm.singleobjective.mos.SolisAndWetsBuilder;
 import org.uma.jmetal.algorithm.singleobjective.mos.SolisAndWetsTecnique;
 import org.uma.jmetal.algorithm.singleobjective.mts.MTS_LS1Builder;
-import org.uma.jmetal.algorithm.singleobjective.mts.MultipleTrajectorySearchBuilder;
 import org.uma.jmetal.problem.DoubleProblem;
 import org.uma.jmetal.solution.DoubleSolution;
 import org.uma.jmetal.util.AlgorithmRunner;
 import org.uma.jmetal.util.ProblemUtils;
+import org.uma.jmetal.util.comparator.FrobeniusComparator;
 import org.uma.jmetal.util.pseudorandom.JMetalRandom;
 
 public class MOSRunner 
@@ -27,29 +25,30 @@ public class MOSRunner
         MOSHRH algorithm;
         String problemName ;
         
-        problemName = "co.edu.unicauca.problem.training_testing.Banknote";
+        problemName = "co.edu.unicauca.problem.training_testing.Blood";
         problem = (DoubleProblem) ProblemUtils.<DoubleSolution> loadProblem(problemName);
         
         
                 
-        MTSLS1Tecnique mts_exec = new MTSLS1Tecnique(new MTS_LS1Builder(problem)
+        MTSLS1Tecnique mtsls1_exec = new MTSLS1Tecnique(new MTS_LS1Builder(problem)
                         .setPopulationSize(5)
                         .setBonus1(10)
                         .setBonus2(1));
                     
-        SolisAndWetsTecnique sw_exec = new SolisAndWetsTecnique(new SolisAndWetsBuilder(problem)
-                        .setRho(0.5)
-                        .setSizeNeighborhood(12));
+        SolisAndWetsTecnique sw_exec1 = new SolisAndWetsTecnique(new SolisAndWetsBuilder(problem)
+            .setRho(0.4)
+            .setSizeNeighborhood(12));
         
         for(int i = 0; i < EXECUTIONS;i++)
         {
             algorithm = new MOSBuilder(problem)
-                        .addTecnique(mts_exec)
-                        .addTecnique(sw_exec)
-                        .setFE(75)
-                        .setE(0.15)
-                        .setMaxEvaluations(3000)
-                        .build(); 
+                            .addTecnique(mtsls1_exec)
+                            .addTecnique(sw_exec1)
+                            .setFE(75)
+                            .setE(0.15)
+                            .setMaxEvaluations(300)
+                            .setComparator(new FrobeniusComparator<>(FrobeniusComparator.Ordering.DESCENDING, FrobeniusComparator.Ordering.ASCENDING, 0))
+                            .build();
             rnd.setSeed(1);
             System.out.println("-------------------------------------------------");
             AlgorithmRunner algorithmRunner = new AlgorithmRunner.Executor(algorithm)
@@ -59,7 +58,7 @@ public class MOSRunner
             long computingTime = algorithmRunner.getComputingTime() ;
 
             System.out.println("Total execution time: " + computingTime + "ms");
-            System.out.println("Objective "+(1-solution.getObjective(0)));
+            System.out.println("Objective "+(solution.getObjective(0)));
             AbstractELMEvaluator p = (AbstractELMEvaluator)problem;
             System.out.println("Testing: "+p.test(solution));
         }
