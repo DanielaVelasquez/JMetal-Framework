7daab6fb7e37018f7cb5c5ad759bbb931ccb688a
DanielaVG <angiedanielav@unicauca.edu.co>
2017-12-08 15:53:15 -0500

    Parametrización solis

--- jMetal-jmetal-5.2/unicauca-ann/src/main/java/co/edu/unicauca/factory/SolisAndWetsFactory.java
+++ jMetal-jmetal-5.2/unicauca-ann/src/main/java/co/edu/unicauca/factory/SolisAndWetsFactory.java
@@ -12,8 +12,8 @@ import org.uma.jmetal.util.JMetalException;
  */
 public class SolisAndWetsFactory extends AbstractFactory
 {
-    private final static double RHO = 0.4;
-    private final static int NEIGHBORHOOD = 12;
+    private final static double RHO = 0.6;
+    private final static int NEIGHBORHOOD = 16;
 
     @Override
     public AlgorithmBuilder getAlgorithm(String name, AbstractELMEvaluator.EvaluatorType evaluatorType, 
