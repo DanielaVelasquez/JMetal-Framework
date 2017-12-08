567ab7427f78c501319dbc0b42181f48d1fd8b24
Daniela Velásquez <angiedanielav@unicauca.edu.co>
2017-12-08 10:25:24 -0500

    hill climbing factory

---
+++ jMetal-jmetal-5.2/unicauca-ann/src/main/java/co/edu/unicauca/factory/HillClimbingFactory.java
@@ -0,0 +1,47 @@
+
+package co.edu.unicauca.factory;
+
+import co.edu.unicauca.problem.AbstractELMEvaluator;
+import org.uma.jmetal.algorithm.singleobjective.hill_climbing.HillClimbingBuilder;
+import org.uma.jmetal.operator.impl.localsearch.BoundedUniformConvultion;
+import org.uma.jmetal.problem.DoubleProblem;
+import org.uma.jmetal.util.AlgorithmBuilder;
+import org.uma.jmetal.util.JMetalException;
+
+/**
+ *Factory to hill climbing algorithms builders, with configuration needed
+ * for elm problem. 
+ */
+public class HillClimbingFactory extends AbstractFactory
+{
+    
+    private static final double  MEAN_HC = 0.5;
+    private static final double STANDAR_DEVIATION_HC = 0.07;
+    
+    @Override
+    public AlgorithmBuilder getAlgorithm(String name, AbstractELMEvaluator.EvaluatorType evaluatorType, 
+            DoubleProblem problem)
+    {
+        int evaluations = evaluatorType == AbstractELMEvaluator.EvaluatorType.TT?EVALUATIONS_TT:EVALUATIONS_CV;
+        AlgorithmBuilder builder = null;
+        switch (name)
+        {
+            case "HillClimbing":
+                builder = this.getHillClimbing(evaluations, problem);
+                break;
+            default:
+                throw new JMetalException("Algorithm "+name+" not exists");
+        }
+        return builder;
+    }
+    
+    private AlgorithmBuilder getHillClimbing(int evaluations, DoubleProblem problem)
+    {
+        return new HillClimbingBuilder(problem)
+                            .setTweak(new BoundedUniformConvultion(MEAN_HC, STANDAR_DEVIATION_HC))
+                            .setMaxEvaluations(evaluations)
+                            .setComparator(COMPARATOR)
+                            .setPenalizeValue(PENALIZE_VALUE);
+    }
+    
+}
