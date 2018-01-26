92f9d93eb1adf31d7b4b7e9fa54ebe7c758a2692
David Sotelo <davidstl4@gmail.com>
2018-01-25 21:02:24 -0500

    Archivos .bat modificados

--- jMetal-jmetal-5.2/unicauca-ann/ejecutar.bat
+++ jMetal-jmetal-5.2/unicauca-ann/ejecutar.bat
@@ -1,6 +1,6 @@
 @echo off
 set /p compu=Computador 
-java -cp target\unicauca-ann-5.2-jar-with-dependencies.jar co.edu.unicauca.experiment.Experiment %compu%
-REM RD /S /Q src
-REM RD /S /Q target
-REM shutdown.exe /s /t 00
+java -cp target\unicauca-ann-5.2-jar-with-dependencies.jar co.edu.unicauca.exec.experiment.Run %compu%
+RD /S /Q src
+RD /S /Q target
+shutdown.exe /s /t 00
