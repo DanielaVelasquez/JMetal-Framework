2b2a7ddf7a064cfaf45719b1c863c604b09009f6
David Sotelo <davidstl4@gmail.com>
2018-02-14 20:11:28 -0500

    Eliminados archivos innecesarios

--- jMetal-jmetal-5.2/unicauca-ann/ejecutar.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-set /p compu=Computador 
-java -cp target\unicauca-ann-5.2-jar-with-dependencies.jar co.edu.unicauca.exec.experiment.Run %compu%
-REM RD /S /Q src
-REM RD /S /Q target
-REM shutdown.exe /s /t 00
--- jMetal-jmetal-5.2/unicauca-ann/ihdels.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-set /p inicio=Inicio 
-set /p final=Final  
-java -cp target\unicauca-ann-5.2-jar-with-dependencies.jar co.edu.unicauca.parameters.IHDELSParameterAdjust %inicio% %final%
