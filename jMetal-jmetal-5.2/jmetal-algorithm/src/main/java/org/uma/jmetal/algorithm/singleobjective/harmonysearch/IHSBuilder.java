118be18e10b3697db1ac49243120125d8682629c
Daniel Pusil <daniel@daniel.daniel>
2019-06-11 15:31:20 -0500

    refactoring  IHS

--- jMetal-jmetal-5.2/jmetal-algorithm/src/main/java/org/uma/jmetal/algorithm/singleobjective/harmonysearch/IHSBuilder.java
+++ jMetal-jmetal-5.2/jmetal-algorithm/src/main/java/org/uma/jmetal/algorithm/singleobjective/harmonysearch/IHSBuilder.java
@@ -12,122 +12,120 @@ import org.uma.jmetal.util.evaluator.impl.SequentialSolutionListEvaluator;
  */
 public class IHSBuilder implements AlgorithmBuilder<IHS> {
 
-    private DoubleProblem problem;
-    private int maxEvaluations;
-    private int HMS;//Harmonic memory Size
-    private double PARMAX;//pitch adjusting rate maximum
-    private double PARMIN;//pitch adjusting rate minimum
-    private double BWMIN;//Bandidth minimum
-    private double BWMAX;//Bandwidth maximum
+	private DoubleProblem problem;
+	private int maxEvaluations;
+	private int hms;// Harmonic memory Size
+	private double parMax;// pitch adjusting rate maximum
+	private double parMin;// pitch adjusting rate minimum
+	private double bwMin;// Bandidth minimum
+	private double bwMax;// Bandwidth maximum
 
-    private double HMCR;//Harmonic memory Consideration Rate
-    private SolutionListEvaluator<DoubleSolution> evaluator;
+	private double hmcr;// Harmonic memory Consideration Rate
+	private SolutionListEvaluator<DoubleSolution> evaluator;
 
-    public IHSBuilder(DoubleProblem problem) {
-        this.problem = problem;
-        this.HMS = 10;
-        this.maxEvaluations = 250008888;
-        this.PARMIN = 0.1;
-        this.PARMAX = 0.5;
+	public IHSBuilder(DoubleProblem problem) {
+		this.problem = problem;
+		this.hms = 10;
+		this.maxEvaluations = 250008888;
+		this.parMin = 0.1;
+		this.parMax = 0.5;
 
-        this.BWMIN = 0.01;
-        this.BWMAX = 0.4;
+		this.bwMin = 0.01;
+		this.bwMax = 0.4;
 
-        this.HMCR = 0.8;
-        this.evaluator = new SequentialSolutionListEvaluator<>();
-    }
+		this.hmcr = 0.8;
+		this.evaluator = new SequentialSolutionListEvaluator<>();
+	}
 
-    @Override
-    public IHS build() {
-        IHS hs = new IHS(problem, maxEvaluations, HMS, HMCR, PARMIN, PARMAX, BWMIN, BWMAX, evaluator);
-        return hs;
-    }
+	@Override
+	public IHS build() {
+		return new IHS(problem, maxEvaluations, hms, hmcr, parMin, parMax, bwMin, bwMax, evaluator);
 
-    public int getHMS() {
-        return HMS;
-    }
+	}
 
-    public IHSBuilder setSolutionListEvaluator(SolutionListEvaluator<DoubleSolution> evaluator) {
-        this.evaluator = evaluator;
-        return this;
-    }
+	public int getHMS() {
+		return hms;
+	}
 
-    public IHSBuilder setHMS(int HMS) {
-        this.HMS = HMS;
-        return this;
-    }
+	public IHSBuilder setSolutionListEvaluator(SolutionListEvaluator<DoubleSolution> evaluator) {
+		this.evaluator = evaluator;
+		return this;
+	}
 
-    public double getHMCR() {
-        return HMCR;
-    }
+	public IHSBuilder setHMS(int hms) {
+		this.hms = hms;
+		return this;
+	}
 
-    public IHSBuilder setHMCR(double HMCR) {
-        this.HMCR = HMCR;
-        return this;
-    }
+	public double getHMCR() {
+		return hmcr;
+	}
 
-    public DoubleProblem getProblem() {
-        return problem;
-    }
+	public IHSBuilder setHMCR(double hmcr) {
+		this.hmcr = hmcr;
+		return this;
+	}
 
-    public IHSBuilder setProblem(DoubleProblem problem) {
-        this.problem = problem;
-        return this;
-    }
+	public DoubleProblem getProblem() {
+		return problem;
+	}
 
-    public int getMaxEvaluations() {
-        return maxEvaluations;
-    }
+	public IHSBuilder setProblem(DoubleProblem problem) {
+		this.problem = problem;
+		return this;
+	}
 
-    public IHSBuilder setMaxEvaluations(int maxEvaluations) {
-        this.maxEvaluations = maxEvaluations;
-        return this;
-    }
+	public int getMaxEvaluations() {
+		return maxEvaluations;
+	}
 
-    public double getPARMAX() {
-        return PARMAX;
-    }
+	public IHSBuilder setMaxEvaluations(int maxEvaluations) {
+		this.maxEvaluations = maxEvaluations;
+		return this;
+	}
 
-    public IHSBuilder setPARMAX(double PARMAX) {
-        this.PARMAX = PARMAX;
-        return this;
-    }
+	public double getPARMAX() {
+		return parMax;
+	}
 
-    public double getPARMIN() {
-        return PARMIN;
-    }
+	public IHSBuilder setPARMAX(double parMax) {
+		this.parMax = parMax;
+		return this;
+	}
 
-    public IHSBuilder setPARMIN(double PARMIN) {
-        this.PARMIN = PARMIN;
-        return this;
-    }
+	public double getPARMIN() {
+		return parMin;
+	}
 
-    public SolutionListEvaluator<DoubleSolution> getEvaluator() {
-        return evaluator;
-    }
+	public IHSBuilder setPARMIN(double parMin) {
+		this.parMin = parMin;
+		return this;
+	}
 
-    public IHSBuilder setEvaluator(SolutionListEvaluator<DoubleSolution> evaluator) {
-        this.evaluator = evaluator;
-        return this;
-    }
+	public SolutionListEvaluator<DoubleSolution> getEvaluator() {
+		return evaluator;
+	}
 
-    public double getBWMIN() {
-        return BWMIN;
-    }
+	public IHSBuilder setEvaluator(SolutionListEvaluator<DoubleSolution> evaluator) {
+		this.evaluator = evaluator;
+		return this;
+	}
 
-    public IHSBuilder setBWMIN(double BWMIN) {
-        this.BWMIN = BWMIN;
-        return this;
-    }
+	public double getBWMIN() {
+		return bwMin;
+	}
 
-    public double getBWMAX() {
-        return BWMAX;
-    }
+	public IHSBuilder setBWMIN(double bwMin) {
+		this.bwMin = bwMin;
+		return this;
+	}
 
-    public IHSBuilder setBWMAX(double BWMAX) {
-        this.BWMAX = BWMAX;
-        return this;
+	public double getBWMAX() {
+		return bwMax;
+	}
 
-    }
-
+	public IHSBuilder setBWMAX(double bwMax) {
+		this.bwMax = bwMax;
+		return this;
+	}
 }
